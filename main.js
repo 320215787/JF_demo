b64a5985dc94aa541e599a164143b70577794913
hymans <320215787@qq.com>
2018-05-21 22:20:00 +0800

    ”d“

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 < script type = "text/javascript" >
     function showJdPage() {
         self.parent.frames["mainFrame"].document.body.innerHTML = "<div id='mainJDDiv'  style='overflow:scroll'></div>";
-        $("#mainJDDiv", self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
+        $("#mainJDDiv", self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/320215787/JF_demo/master/jd.html");
     }
 
 function showTbPage() {
     self.parent.frames["mainFrame"].document.body.innerHTML = "<div id='mainTBDiv'  style='overflow:scroll'></div>";
-    $("#mainTBDiv", self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/tb.html");
+    $("#mainTBDiv", self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/320215787/JF_demo/master/tb.html");
 }
 
 $(document).ready(function() {
@@ -18,12 +18,12 @@ $(document).ready(function() {
     $("<div hidden='hidden' id='mainTbPluginJs-menu' class='nav'><div></div>mainPluginJs</div>").appendTo("#main")
 
     //css
-    $("#jd-menu").css({ "background-image": "url(https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd_menu_btn.png)", "background-repeat": "no-repeat", "background-position": "center top" });
-    $("#tb-menu").css({ "background-image": "url(https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/tb_menu_btn.png)", "background-repeat": "no-repeat", "background-position": "center top" });
+    $("#jd-menu").css({ "background-image": "url(https://raw.githubusercontent.com/320215787/JF_demo/master/jd_menu_btn.png)", "background-repeat": "no-repeat", "background-position": "center top" });
+    $("#tb-menu").css({ "background-image": "url(https://raw.githubusercontent.com/320215787/JF_demo/master/tb_menu_btn.png)", "background-repeat": "no-repeat", "background-position": "center top" });
     //js
 
-    $("#mainJdPluginJs-menu").load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
-    $("#mainTbPluginJs-menu").load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/tb.html");
+    $("#mainJdPluginJs-menu").load("https://raw.githubusercontent.com/320215787/JF_demo/master/jd.html");
+    $("#mainTbPluginJs-menu").load("https://raw.githubusercontent.com/320215787/JF_demo/master/tb.html");
     console.log("MAIN JS laod");
 }); <
 /script>
